932f9f005e3ef5484ba9b3c7e71f00cdc82d2d2e
bloginfotech <88288940+bloginfotech@users.noreply.github.com>
2021-08-02 18:37:53 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ mkdir ~/wrk/test
 cat > ~/bin/efitest.sh <<EOF
 #!/bin/bash
 
-qemu-system-x86_64 -bios /usr/share/ovmf/OVMF.fd -drive file=fat:rw:~/wrk/test -boot c -nic none
+qemu-system-x86_64 -bios /usr/share/ovmf/OVMF.fd -drive file=fat:rw:~/wrk/test -boot c -nic none -usb -device usb-mouse
 
 EOF
 chmod +x ~/bin/efitest.sh
